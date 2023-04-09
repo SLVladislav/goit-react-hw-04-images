ac6613c20eb8168412456dbb5deb106204defd62
SLVladislav <bakalavrat.789@gmail.com>
2023-04-09 17:08:10 +0300

    image gallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,25 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
-import { ImageGallery } from './ImageGallery/ImageGallery';
+import ImageGallery from './ImageGallery/ImageGallery';
 import { Container } from './App.styled';
 import { MyGlobalStyles } from 'globalStyles/GlobalStyles.styled';
 import { ToastContainer } from 'react-toastify';
 
-export class App extends Component {
-  state = {
-    imgTheme: '',
+export default function App() {
+  const [imgTheme, setImgTheme] = useState('');
+
+  const handleFormSubmit = imgTheme => {
+    setImgTheme(imgTheme);
   };
 
-  handleFormSubmit = imgTheme => {
-    this.setState({ imgTheme });
-  };
-
-  render() {
-    return (
-      <div>
-        <Container>
-          <MyGlobalStyles />
-          <Searchbar onSubmit={this.handleFormSubmit} />
-          <ImageGallery imgTheme={this.state.imgTheme} />
-          <ToastContainer />
-        </Container>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Container>
+        <MyGlobalStyles />
+        <Searchbar onSubmit={handleFormSubmit} />
+        <ImageGallery imgTheme={imgTheme} />
+        <ToastContainer />
+      </Container>
+    </div>
+  );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Loader } from 'components/Loader/Loader';
 import { fetchImg } from 'services/Api';
@@ -14,101 +14,122 @@ const Status = {
   REJECTED: 'rejected',
 };
 
-export class ImageGallery extends Component {
-  static propTypes = {
-    imgTheme: PropTypes.string.isRequired,
-  };
+export default function ImageGallery({ imgTheme }) {
+  // state = {
+  //   images: [],
+  //   error: null,
+  //   status: Status.IDLE,
+  //   page: null,
+  //   totalHits: null,
+  // };
 
-  state = {
-    images: [],
-    error: null,
-    status: Status.IDLE,
-    page: null,
-    totalHits: null,
-  };
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState('');
+  const [status, setStatus] = useState(Status.IDLE);
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+  const [search, setSearch] = useState('');
 
-  async componentDidUpdate(prevProps, prevState) {
+  // async componentDidUpdate(prevProps, prevState) {
+  //   if (
+  //     this.state.status === Status.RESOLVED &&
+  //     this.state.images.length === this.state.totalHits &&
+  //     prevState.images.length !== this.state.totalHits
+  //   ) {
+  //     toast.warn('Sorry, there are no images matching your search query.');
+  //   }
+  //   const { imgTheme } = this.props;
+
+  //   if (imgTheme && prevProps.imgTheme !== imgTheme) {
+  //     await this.setState({
+  //       images: [],
+  //       page: 1,
+  //     });
+
+  //     await this.loadImg();
+  //   }
+  // }
+
+  useEffect(() => {
+    if (!imgTheme) return;
+
+    setSearch(imgTheme);
+    setImages([]);
+    setPage(1);
+  }, [imgTheme]);
+
+  useEffect(() => {
+    if (!search) return;
+
+    async function loadImg() {
+      setStatus(Status.PENDING);
+
+      try {
+        const { hits, totalHits } = await fetchImg(imgTheme, page);
+
+        const normalizedImages = hits.map(
+          ({ id, webformatURL, largeImageURL, tags }) => {
+            return {
+              id,
+              webformatURL,
+              largeImageURL,
+              tags,
+            };
+          }
+        );
+
+        setImages(prevState => [...prevState, ...normalizedImages]);
+        setError('');
+        setStatus(Status.RESOLVED);
+        setTotalHits(totalHits);
+      } catch (error) {
+        setError('error');
+        setStatus(Status.REJECTED);
+      }
+    }
+    loadImg();
+  }, [search, page]);
+
+  useEffect(() => {
     if (
-      this.state.status === Status.RESOLVED &&
-      this.state.images.length === this.state.totalHits &&
-      prevState.images.length !== this.state.totalHits
+      status === Status.RESOLVED &&
+      images.length === totalHits &&
+      images.length !== totalHits
     ) {
       toast.warn('Sorry, there are no images matching your search query.');
     }
-    const { imgTheme } = this.props;
+  }, [images.length, totalHits]);
 
-    if (imgTheme && prevProps.imgTheme !== imgTheme) {
-      await this.setState({
-        images: [],
-        page: 1,
-      });
-
-      await this.loadImg();
-    }
-  }
-
-  loadImg = async () => {
-    this.setState({ status: Status.PENDING });
-
-    try {
-      const { hits, totalHits } = await fetchImg(
-        this.props.imgTheme,
-        this.state.page
-      );
-
-      await this.setState(prevState => {
-        return {
-          images: [...prevState.images, ...normalizedImages],
-          error: null,
-          status: Status.RESOLVED,
-          page: prevState.page + 1,
-          totalHits: totalHits,
-        };
-      });
-
-      const normalizedImages = hits.map(
-        ({ id, webformatURL, largeImageURL, tags }) => {
-          return {
-            id,
-            webformatURL,
-            largeImageURL,
-            tags,
-          };
-        }
-      );
-    } catch (error) {
-      this.setState({ error: 'error', status: Status.REJECTED });
-    }
-  };
-
-  render() {
-    const { status, images, totalHits } = this.state;
-
-    return (
-      <>
-        {images.length > 0 && (
-          <ImageGalleryList className="gallery">
-            {images.map(image => {
-              return <ImageGalleryItem key={image.id} image={image} />;
-            })}
-          </ImageGalleryList>
-        )}
-        {!(images.length >= totalHits) && status === Status.RESOLVED && (
-          <ButtonLoadMore type="button" onClick={() => this.loadImg()}>
-            Load More
-          </ButtonLoadMore>
-        )}
-        {status === Status.PENDING && <Loader />}
-        {status === Status.REJECTED && (
-          <h2>
-            An error occurred, we could not upload the photo, please try
-            reloading the page and try again :)
-          </h2>
-        )}
-        {images.length === 0 && status === Status.RESOLVED && (
-          <h2>We didn't find anything according to your request</h2>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      {images.length > 0 && (
+        <ImageGalleryList className="gallery">
+          {images.map(({ id, ...restProps }) => {
+            return <ImageGalleryItem key={id} image={restProps} />;
+          })}
+        </ImageGalleryList>
+      )}
+      {!(images.length >= totalHits) && status === Status.RESOLVED && (
+        <ButtonLoadMore
+          type="button"
+          onClick={() => setPage(prevState => prevState + 1)}
+        >
+          Load More
+        </ButtonLoadMore>
+      )}
+      {status === Status.PENDING && <Loader />}
+      {status === Status.REJECTED && (
+        <h2>
+          An error occurred, we could not upload the photo, please try reloading
+          the page and try again :)
+        </h2>
+      )}
+      {images.length === 0 && status === Status.RESOLVED && (
+        <h2>We didn't find anything according to your request</h2>
+      )}
+    </>
+  );
 }
+ImageGallery.propTypes = {
+  imgTheme: PropTypes.string.isRequired,
+};
